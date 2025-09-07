bf1740f4bec8c505ab066f678860ead2a2ecd1eb
RintaroNasu <rinta022823@gmail.com>
2025-09-07 20:37:06 +0900

    feat: :sparkles: ホームページUIの作成

--- frontend/lib/pages/home/home_page.dart
+++ frontend/lib/pages/home/home_page.dart
@@ -1,10 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text('ホーム'));
+    return Padding(
+      padding: const EdgeInsets.all(16),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(
+            width: double.infinity,
+            height: 100,
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: const Color.fromARGB(255, 10, 114, 198),
+                foregroundColor: Colors.white,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12),
+                ),
+              ),
+              onPressed: () => context.go('/record'),
+              child: const Text(
+                '今日の記録を始める',
+                style: TextStyle(fontSize: 30, fontWeight: FontWeight.w900),
+              ),
+            ),
+          ),
+          const SizedBox(height: 30),
+          const Text(
+            '前回の記録',
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(height: 16),
+          // 前回の記録を表示
+          const SizedBox(height: 30),
+          const Text(
+            'サマリー',
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          ),
+        ],
+      ),
+    );
   }
 }
