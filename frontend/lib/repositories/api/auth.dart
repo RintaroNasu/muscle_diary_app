a6816260009540c8757eb3d3319aa96b510c29d5
RintaroNasu <rinta022823@gmail.com>
2025-09-08 19:29:05 +0900

    feat: :sparkles: api接続メソッド作成

---
+++ frontend/lib/repositories/api/auth.dart
@@ -0,0 +1,31 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+const _baseUrl = 'http://localhost:8080';
+const _tokenKey = 'token';
+final _storage = const FlutterSecureStorage();
+
+Future<String?> readStoredToken() => _storage.read(key: _tokenKey);
+
+Future<String?> loginApi(String email, String password) async {
+  final response = await http.post(
+    Uri.parse('$_baseUrl/login'),
+    body: jsonEncode({'email': email, 'password': password}),
+  );
+  if (response.statusCode == 200) {
+    return response.body;
+  }
+  return null;
+}
+
+Future<String?> signupApi(String email, String password) async {
+  final response = await http.post(
+    Uri.parse('$_baseUrl/signup'),
+    body: jsonEncode({'email': email, 'password': password}),
+  );
+  if (response.statusCode == 200) {
+    return response.body;
+  }
+  return null;
+}
