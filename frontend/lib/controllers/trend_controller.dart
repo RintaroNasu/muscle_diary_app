389651ab1163bea47f985faf8d48fb3522902fa4
RintaroNasu <rinta022823@gmail.com>
2025-10-21 12:22:08 +0900

    feat: :sparkles: 推移のロジック部分の実装

---
+++ frontend/lib/controllers/trend_controller.dart
@@ -0,0 +1,18 @@
+import 'package:frontend/models/workout_set_item.dart';
+import 'package:frontend/repositories/api/trend.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+final selectedExerciseIdProvider = StateProvider<int?>((ref) => null);
+
+final exerciseSetsProvider = FutureProvider.autoDispose
+    .family<List<WorkoutSetItem>, String>((ref, exerciseId) async {
+      final id = int.parse(exerciseId);
+      final items = await fetchWorkoutSetsByExercise(id);
+
+      items.sort((a, b) {
+        final c = a.trainedOn.compareTo(b.trainedOn);
+        if (c != 0) return c;
+        return a.setNo.compareTo(b.setNo);
+      });
+      return items;
+    });
