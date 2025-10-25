13acebd03152d41d7e536b75098397bd63a3f85f
RintaroNasu <rinta022823@gmail.com>
2025-10-25 17:33:14 +0900

    fix: :recycle: errorを別ファイルに切り分ける

---
+++ backend/internal/service/errors.go
@@ -0,0 +1,18 @@
+package service
+
+import "errors"
+
+// 共通（Userドメインで再利用可能）
+var (
+	ErrUserAlreadyExists  = errors.New("user already exists")
+	ErrUserNotFound       = errors.New("user not found")
+	ErrInvalidCredentials = errors.New("invalid credentials")
+)
+
+// Workoutドメインで利用可能
+var (
+	ErrNoSets           = errors.New("no sets")
+	ErrInvalidSetValue  = errors.New("invalid set value")
+	ErrExerciseNotFound = errors.New("exercise not found")
+	ErrRecordNotFound   = errors.New("record not found")
+)
