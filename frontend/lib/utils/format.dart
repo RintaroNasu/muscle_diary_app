2c4d0973d7200ac5979cd7000985dd28b892c9c9
RintaroNasu <rinta022823@gmail.com>
2025-11-06 19:56:44 +0900

    refactor: :recycle: メソッドを別ファイルに切り分け

--- frontend/lib/utils/format.dart
+++ frontend/lib/utils/format.dart
@@ -2,3 +2,5 @@ String two(int n) => n.toString().padLeft(2, '0');
 
 String kg(double v) =>
     '${v % 1 == 0 ? v.toStringAsFixed(0) : v.toStringAsFixed(1)}kg';
+
+String ymd(DateTime d) => '${d.year}/${two(d.month)}/${two(d.day)}';
