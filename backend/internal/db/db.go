43e431029f0c27ff599f47bb887aea17a5a3c941
RintaroNasu <rinta022823@gmail.com>
2025-11-25 19:15:57 +0900

    feat: :zap: スクリプトから種目登録していたのをseedファイル作成してサーバー起動時に登録するようにする

--- backend/internal/db/db.go
+++ backend/internal/db/db.go
@@ -10,16 +10,21 @@ import (
 )
 
 func New() (*gorm.DB, error) {
-	err := godotenv.Load()
-	if err != nil {
-		return nil, fmt.Errorf("failed to load .env file: %w", err)
+	_ = godotenv.Load()
+	dsn := os.Getenv("DATABASE_URL")
+
+	if dsn == "" {
+		dsn = fmt.Sprintf("postgres://%s:%s@%s:%s/%s", os.Getenv("POSTGRES_USER"),
+			os.Getenv("POSTGRES_PASSWORD"), os.Getenv("POSTGRES_HOST"),
+			os.Getenv("POSTGRES_PORT"), os.Getenv("POSTGRES_DB"))
 	}
 
-	dbUrl := fmt.Sprintf("postgres://%s:%s@%s:%s/%s", os.Getenv("POSTGRES_USER"),
-		os.Getenv("POSTGRES_PASSWORD"), os.Getenv("POSTGRES_HOST"),
-		os.Getenv("POSTGRES_PORT"), os.Getenv("POSTGRES_DB"))
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		return nil, fmt.Errorf("failed to connect database: %w", err)
+	}
 
-	return (gorm.Open(postgres.Open(dbUrl), &gorm.Config{}))
+	return db, nil
 }
 
 func CloseDB(db *gorm.DB) error {
